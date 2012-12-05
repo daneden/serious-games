0b6f2d78082946b65630fcf0225a4e62bdb647d4
Dan Eden <dan.eden@me.com>
2012-12-05 23:08:11 +0000

    Tweak the home page hero banner

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php require_once('inc/header.php'); ?>
 <div class="wrap">
-	<div class="home-hero">
-		<img src="/assets/images/promo.jpg" class="hero-banner">
+	<div class="home-hero island">
 		<div class="hero-intro">
 			<h1>Welcome to Gateway</h1>
 			<p class="intro"><strong>Gateway</strong> is a website for learning the vocational skills you need to find a job. Start now by <a href="/signup">signing up</a>.</p>
