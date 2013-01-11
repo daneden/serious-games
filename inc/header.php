3b4a13b59d75627bce195acf0688e0a2afa0a883
Dan Eden <dan.eden@me.com>
2013-01-11 13:23:07 +0000

    Use relative paths for assets

--- inc/header.php
+++ inc/header.php
@@ -3,14 +3,14 @@
 <head>
 	<meta charset="utf-8">
 	<title>Gateway</title>
-	<link rel="stylesheet" href="/assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/style.css">
 	<script type="text/javascript" src="//use.typekit.net/cnh2osh.js"></script>
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 </head>
 <body>
 <header class="site-header">
 	<div class="wrap isle cf">
-		<a href="/" title="Gateway Home" class="site-title"><img width="132" height="34" src="/assets/images/site-logo@2x.png" alt="Gateway logo"></a>
+		<a href="/" title="Gateway Home" class="site-title"><img width="132" height="34" src="assets/images/site-logo@2x.png" alt="Gateway logo"></a>
 		<nav class="user-nav">
 			<a class="user-link" href="/user">
 				<span class="user-name">Dan Eden</span>
