3f25c187efbe03527b09c9ecf36828f0b6fc508e
Dan Eden <dan.eden@me.com>
2013-01-31 21:32:59 +0000

    Complete lesson page (without media)

--- lesson.php
+++ lesson.php
@@ -3,10 +3,27 @@
 	<div class="content two-col">
 		<div class="main-col island">
 			<h1>Vocational Language</h1>
-			<span class="gamma">Lesson 1: <strong>Introduction</strong></span>
-			<p>Hi there! You&rsquo;re only a few minutes away from learning the vocational skills you need to get a job. Complete the form below and we&rsquo;ll do the rest.</p>
+			<span class="gamma lesson-header">Lesson 1: <strong class="lesson-title">Introduction</strong></span>
 			<div class="p lesson-progress">
 				<div class="progress-measure" style="width: 33%;" data-tooltip="33% Complete"><span class="visually-hidden">33% Complete</span></div>
+			</div>
+
+			<div class="lesson-content">
+				<div class="lesson-question">
+					<div class="question-header island">
+						<h2>Question 2 <span class="questions-total">of 3</span></h2>
+						<p class="intro standalone">Lorem ipsum dolor sit amet, consectetur adipisicing elit?</p>
+					</div>
+					<ol class="question-answer-group standalone">
+						<li class="question-answer isle selected-answer"><input checked class="alignleft qa-in" type="radio" value="q2-a1" name="q2" id="q2-a1"><label for="q2-a1">Lorem Ipsum</label></li>
+						<li class="question-answer isle"><input class="alignleft qa-in" type="radio" value="q2-a2" name="q2" id="q2-a2"><label for="q2-a2">Dolor Sit</label></li>
+						<li class="question-answer isle"><input class="alignleft qa-in" type="radio" value="q2-a3" name="q2" id="q2-a3"><label for="q2-a3">Amet Consectetur</label></li>
+					</ol>
+				</div>
+				<div class="lesson-navigation cf island">
+					<button class="butt butt-primary alignright">Next Question</button>
+					<button class="butt alignleft">Go Back</button>
+				</div>
 			</div>
 		</div>
 		<div class="sidebar secondary-col island">
