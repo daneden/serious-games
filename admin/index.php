0a272603cbae5d5b5f3657f04351473ac8d509a8
Dan Eden <dan.eden@me.com>
2013-02-21 16:53:03 +0000

    Add more admin styles, edit index page

--- admin/index.php
+++ admin/index.php
@@ -3,18 +3,8 @@
 	require_once('../inc/header.php');
 ?>
 <div class="wrap">
-	<div class="home-hero island">
-		<div class="hero-intro">
-			<h1>Welcome to Gateway</h1>
-			<p class="intro"><strong>Gateway</strong> is a website for learning the vocational skills you need to find a job. Start now by <a href="/signup">signing up</a>.</p>
-		</div>
-	</div>
 	<div class="content two-col">
-		<div class="main-col island">
-			<h1>Hi there, Dan!</h1>
-			<p>Welcome to your dashboard! From here, you can view available challenges, retake challenges you&rsquo;ve previously completed, and a load of other junk.</p>
-		</div>
-		<div class="sidebar secondary-col island">
+		<div class="sidebar bordered-col secondary-col island">
 			<h2>Page Designs</h2>
 			<ul>
 				<li><a href="/dashboard.php">Dashboard</a></li>
@@ -22,6 +12,10 @@
 				<li><a href="/lesson.php">Lesson</a></li>
 				<li><a href="/styleguide.php">Styleguide</a></li>
 			</ul>
+		</div>
+		<div class="main-col island">
+			<h1>Hi there, Dan!</h1>
+			<p>Welcome to the admin dashboard! From here, you can add new challenges and edit existing ones, remove users and more</p>
 		</div>
 	</div>
 </div>
