53073fa71dc997cfe324605e1b5dbca53a538467
Jonathan Walter <Jon.Walter.89@gmail.com>
2013-02-27 23:14:32 +0000

    Removed sample lesson from dashboard

--- dashboard.php
+++ dashboard.php
@@ -17,15 +17,6 @@
 			<p>Welcome to your dashboard! From here, you can view available challenges, retake challenges you&rsquo;ve previously completed, and a load of other junk.</p>
 			<ul class="challenges">
 				<?php get_categories(); ?>
-				<li class="island">
-					<h2 class="standalone">Vocational Language</h2>
-					<p class="desc">Introduction to vocational English language, common phrases, and more.</p>
-					<ol class="sub-challenges">
-						<li class="challenge-complete"><a href="#">Introduction</a></li>
-						<li class="challenge-complete"><a href="#">Your First Interview</a></li>
-						<li class="challenge-complete"><a href="#">Safety Training</a></li>
-					</ol>
-				</li>
 			</ul>
 		</div>
 		<div class="sidebar secondary-col island">
