e0f523a6ad3ba5c5852173d45153588d6fc894a9
Jonathan Walter <Jon.Walter.89@gmail.com>
2013-03-14 16:54:41 +0000

    Creating module leads to creating questions

--- admin/inc/php/create_new_module.php
+++ admin/inc/php/create_new_module.php
@@ -3,5 +3,6 @@
 	$moduleName = mysql_real_escape_string($_POST['module-name']);
 	$moduleCategoryID = $_POST['module-category'];
 	$createNewModule = mysql_query('INSERT INTO lessontable (lessonID, lessonCategoryID, lessonTitle) VALUES ( NULL, "'.$moduleCategoryID.'","'.$moduleName.'")');
-	header('location:../../modules.php');
+	$moduleID = mysql_insert_id();
+	header('location:../../add_question.php?Mid='.$moduleID);
 ?>
