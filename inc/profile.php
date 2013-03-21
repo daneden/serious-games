207e8817550854c8af00381151e20596d4795bd6
Jonathan Walter <Jon.Walter.89@gmail.com>
2013-03-21 16:57:33 +0000

    Misc minor changes to results and lessons

---
+++ inc/profile.php
@@ -0,0 +1,5 @@
+<h3>Profile Overview</h3>
+    <img class="alignleft avatar" src="http://0.gravatar.com/avatar/<?php echo md5(get_user_email());?>?s=96&d=identicon&r=R" width="48" height="48">
+    <p class="standalone"><strong><?php get_user_fname(); ?> <?php get_user_sname(); ?></strong></p>
+    <p class="standalone"><a href="/edit-profile.php">Edit profile/settings</a></p>
+    <hr>
