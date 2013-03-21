75aea1eeda95bc3d1cbbd4752de306c8e775aeff
Dan Eden <dan.eden@me.com>
2013-03-21 15:27:38 +0000

    Add selection styles, .unavailable min-height, and sidebar content

--- dashboard.php
+++ dashboard.php
@@ -20,8 +20,11 @@
 			</ul>
 		</div>
 		<div class="sidebar secondary-col island">
-			<h2>Profile Overview</h2>
-			<h3><?php get_user_fname(); ?> <?php get_user_sname(); ?></h3>
+			<h3>Profile Overview</h3>
+			<img class="alignleft avatar" src="http://0.gravatar.com/avatar/<?php echo md5(get_user_email());?>?s=96&d=identicon&r=R" width="48" height="48">
+			<p class="standalone"><strong><?php get_user_fname(); ?> <?php get_user_sname(); ?></strong></p>
+			<p class="standalone"><a href="/edit-profile.php">Edit profile/settings</a></p>
+			<hr>
 		</div>
 	</div>
 </div>
