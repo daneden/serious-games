1a085e7d5d7159c94ec2ed4b493f77b16452cc52
Dan Eden <dan.eden@me.com>
2013-04-05 20:54:57 +0100

    Columnate the landing promo paragraphs

--- index.php
+++ index.php
@@ -9,18 +9,25 @@
 	</div>
 	<div class="content two-col">
 		<div class="main-col island">
-			<img src="/assets/images/home-clock@2x.png" width="128" alt="Learn on your own time" class="alignright">
-			<h2>Learn In Your Own Time</h2>
-			<p><em>Gateway is designed to work around you.</em> You choose the kind of work you’re interested in, and the Gateway content creators will make sure that you get the learning materials you need.</p>
-			<p>Take online quizzes and classes in <em>your</em> own time, check your progress, and unlock more advanced stages to help you understand the British workplace, and give you the tools you need to succeed.</p>
+			<p class="intro">Take online quizzes and classes in <em>your</em> own time, check your progress, and unlock more advanced stages to help you understand the British workplace, and give you the tools you need to succeed.</p>
 			<hr>
-			<img src="/assets/images/home-info@2x.png" width="128" alt="Get the information you need" class="alignright">
-			<h2>Serving Suggestions</h2>
-			<p>Gateway works best when coupled with other learning resources. That&rsquo;s why our quizzes will point you in the right direction to other websites where further information can be found, and you can learn even more.</p>
-			<hr>
-			<img src="/assets/images/home-travel@2x.png" width="128" alt="Learn wherever you are" class="alignright">
-			<h2>Learn On The Go</h2>
-			<p>At home, at the library, or on the bus; Gateway helps you learn anywhere you are, whether you’re at a computer or browsing the web on your phone. Gateway also makes it easy for you to pick up where you left off, keeping track of the quizzes you’ve already completed.</p>
+			<div class="landing-promo grid">
+				<div class="unit one-of-three">
+					<img src="/assets/images/home-clock@2x.png" width="128" alt="Learn on your own time">
+					<h4>Learn In Your Own Time</h4>
+					<p><em>Gateway is designed to work around you.</em> You choose the kind of work you’re interested in, and the Gateway content creators will make sure that you get the learning materials you need.</p>
+				</div>
+				<div class="unit one-of-three">
+					<img src="/assets/images/home-info@2x.png" width="128" alt="Get the information you need">
+					<h4>Serving Suggestions</h4>
+					<p>Gateway works best when coupled with other learning resources. That&rsquo;s why our quizzes will point you in the right direction to other websites where further information can be found, and you can learn even more.</p>
+				</div>
+				<div class="unit one-of-three">
+					<img src="/assets/images/home-travel@2x.png" width="128" alt="Learn wherever you are">
+					<h4>Learn On The Go</h4>
+					<p>At home, at the library, or on the bus; Gateway helps you learn anywhere you are, whether you’re at a computer or browsing the web on your phone. Gateway also makes it easy for you to pick up where you left off, keeping track of the quizzes you’ve already completed.</p>
+				</div>
+			</div>
 			<hr>
 			<h2>Start Learning Today</h2>
 			<p>Why wait any longer? <a href="/signup.php">Sign up</a> for a free account today &ndash; it only takes a minute.</p>
