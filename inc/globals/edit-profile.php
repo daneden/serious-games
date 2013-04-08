310259a781f179f8252b3b8b05aafe35f03a5c20
Jonathan Walter <Jon.Walter.89@gmail.com>
2013-04-08 16:09:31 +0100

    Images over 1mb won't be uploaded on profile pic

--- inc/globals/edit-profile.php
+++ inc/globals/edit-profile.php
@@ -3,13 +3,21 @@
 	require_once('functions.php');
   if ($_FILES["profile-pic"]["error"] > 0)
 	{
-	echo "Return Code: " . $_FILES["profile-pic"]["error"] . "<br>";
+	$getImageQuery = mysql_query('SELECT * FROM usertable WHERE userID = "'.$_POST['userID'].'"');
+	$getImage = mysql_fetch_assoc($getImageQuery);
+	$imageName = $getImage['userProfileImg'];
 	}
   else
 	{
+		if (filesize($_FILES["profile-pic"]["tmp_name"]) < 1048576){
 	  $imageName = 'userID'.$_POST['userID'].'.'.pathinfo($_FILES["profile-pic"]["name"], PATHINFO_EXTENSION);
 	  move_uploaded_file($_FILES["profile-pic"]["tmp_name"], getcwd() . '/../../assets/profile-pics/'.$imageName);
 	  crop_image(getcwd() . '/../../assets/profile-pics/'.$imageName, getcwd() . '/../../assets/profile-pics/'.$imageName);
+		} else {
+			$getImageQuery = mysql_query('SELECT * FROM usertable WHERE userID = "'.$_POST['userID'].'"');
+			$getImage = mysql_fetch_assoc($getImageQuery);
+			$imageName = $getImage['userProfileImg'];
+		}
 	}	
 	$userID = $_POST['userID'];
 	$userFName = mysql_real_escape_string($_POST['first-name']);
