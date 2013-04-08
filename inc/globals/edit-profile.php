6950b7e2658de189642de178b3a3976874ca2896
Jonathan Walter <Jon.Walter.89@gmail.com>
2013-04-08 16:46:14 +0100

    Various PHP messages

--- inc/globals/edit-profile.php
+++ inc/globals/edit-profile.php
@@ -1,30 +1,37 @@
 <?php
-	require_once('../db/connect.php');
-	require_once('functions.php');
-  if ($_FILES["profile-pic"]["error"] > 0)
-	{
-	$getImageQuery = mysql_query('SELECT * FROM usertable WHERE userID = "'.$_POST['userID'].'"');
-	$getImage = mysql_fetch_assoc($getImageQuery);
-	$imageName = $getImage['userProfileImg'];
-	}
-  else
-	{
-		if (filesize($_FILES["profile-pic"]["tmp_name"]) < 1048576){
-	  $imageName = 'userID'.$_POST['userID'].'.'.pathinfo($_FILES["profile-pic"]["name"], PATHINFO_EXTENSION);
-	  move_uploaded_file($_FILES["profile-pic"]["tmp_name"], getcwd() . '/../../assets/profile-pics/'.$imageName);
-	  crop_image(getcwd() . '/../../assets/profile-pics/'.$imageName, getcwd() . '/../../assets/profile-pics/'.$imageName);
-		} else {
-			$getImageQuery = mysql_query('SELECT * FROM usertable WHERE userID = "'.$_POST['userID'].'"');
-			$getImage = mysql_fetch_assoc($getImageQuery);
-			$imageName = $getImage['userProfileImg'];
+	if($_SERVER['REQUEST_METHOD'] == 'POST') { 
+		require_once('../db/connect.php');
+		require_once('functions.php');
+		$errors = "none";
+	  if ($_FILES["profile-pic"]["error"] > 0)
+		{
+		$getImageQuery = mysql_query('SELECT * FROM usertable WHERE userID = "'.$_POST['userID'].'"');
+		$getImage = mysql_fetch_assoc($getImageQuery);
+		$imageName = $getImage['userProfileImg'];
+		$errors= $_FILES["profile-pic"]["error"];
 		}
-	}	
-	$userID = $_POST['userID'];
-	$userFName = mysql_real_escape_string($_POST['first-name']);
-	$userSName = mysql_real_escape_string($_POST['last-name']);
-	$userEmail = mysql_real_escape_string($_POST['email']);
-	$userSpecialisation = mysql_real_escape_string(json_encode ($_POST['specialisation']));
-	$userLanguage = $_POST['language'];
-	$updateQuery = mysql_query("UPDATE usertable SET userEmail = '$userEmail', userFName = '$userFName', userSName = '$userSName', userLanguage = '$userLanguage', userSpecialisation = '$userSpecialisation', userProfileImg = '$imageName' WHERE userID = '$userID'");
-	header('location:/edit-profile.php');
+	  else
+		{
+			if (filesize($_FILES["profile-pic"]["tmp_name"]) < 1048576){
+		  $imageName = 'userID'.$_POST['userID'].'.'.pathinfo($_FILES["profile-pic"]["name"], PATHINFO_EXTENSION);
+		  move_uploaded_file($_FILES["profile-pic"]["tmp_name"], getcwd() . '/../../assets/profile-pics/'.$imageName);
+		  crop_image(getcwd() . '/../../assets/profile-pics/'.$imageName, getcwd() . '/../../assets/profile-pics/'.$imageName);
+			} else {
+				$getImageQuery = mysql_query('SELECT * FROM usertable WHERE userID = "'.$_POST['userID'].'"');
+				$getImage = mysql_fetch_assoc($getImageQuery);
+				$imageName = $getImage['userProfileImg'];
+				$errors = 1;
+			}
+		}	
+		$userID = $_POST['userID'];
+		$userFName = mysql_real_escape_string($_POST['first-name']);
+		$userSName = mysql_real_escape_string($_POST['last-name']);
+		$userEmail = mysql_real_escape_string($_POST['email']);
+		$userSpecialisation = mysql_real_escape_string(json_encode ($_POST['specialisation']));
+		$userLanguage = $_POST['language'];
+		$updateQuery = mysql_query("UPDATE usertable SET userEmail = '$userEmail', userFName = '$userFName', userSName = '$userSName', userLanguage = '$userLanguage', userSpecialisation = '$userSpecialisation', userProfileImg = '$imageName' WHERE userID = '$userID'");
+		header('location:/edit-profile.php?errors='.$errors);
+	} else {
+		header ('location:/login.php');	
+	}
 ?>
--- inc/globals/login.php
+++ inc/globals/login.php
@@ -1,28 +1,32 @@
 <?php
-	ob_start();
-	include("../db/connect.php");
-	$EmailAddress = $_POST["email-address"];
-	$EmailAddress = stripslashes($EmailAddress);	
-	$Password = $_POST["password"];
-	$Password = stripslashes($Password);
-	$Password = md5($Password);
-	$chklogin = "SELECT * FROM usertable WHERE userEmail ='$EmailAddress' AND userPassword='$Password'";
-	$result = mysql_query($chklogin);
-	$UserDetails = mysql_fetch_assoc($result);
-	$count = mysql_num_rows($result);
-	
-	if ($count==1){
-		session_start();
-		$_SESSION['UserID'] = $UserDetails['userID'];
-		if($UserDetails['userIsAdmin'] == 1){
-			$_SESSION['AdminState'] = 1;
-			header("location:../../admin/index.php");
+	if($_SERVER['REQUEST_METHOD'] == 'POST') { 
+		ob_start();
+		include("../db/connect.php");
+		$EmailAddress = $_POST["email-address"];
+		$EmailAddress = stripslashes($EmailAddress);	
+		$Password = $_POST["password"];
+		$Password = stripslashes($Password);
+		$Password = md5($Password);
+		$chklogin = "SELECT * FROM usertable WHERE userEmail ='$EmailAddress' AND userPassword='$Password'";
+		$result = mysql_query($chklogin);
+		$UserDetails = mysql_fetch_assoc($result);
+		$count = mysql_num_rows($result);
+		
+		if ($count==1){
+			session_start();
+			$_SESSION['UserID'] = $UserDetails['userID'];
+			if($UserDetails['userIsAdmin'] == 1){
+				$_SESSION['AdminState'] = 1;
+				header("location:../../admin/index.php");
+			} else {
+				$_SESSION['AdminState'] = 0;
+				header("location:../../dashboard.php");
+			}
 		} else {
-			$_SESSION['AdminState'] = 0;
-			header("location:../../dashboard.php");
+			echo "Wrong Username or Password!";	
 		}
+		ob_end_flush();
 	} else {
-		echo "Wrong Username or Password!";	
+		header ('location:/login.php');	
 	}
-	ob_end_flush();
 ?>
