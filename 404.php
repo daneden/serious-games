fe152b43985b36222013273078d0ad3222e0770d
Dan Eden <dan.eden@me.com>
2013-04-08 14:15:55 +0100

    Add header login form, 404 page

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php require_once('inc/header.php'); ?>
+<div class="wrap">
+	<div class="content two-col">
+		<div class="main-col island">
+			<h1>Page Not Found</h1>
+			<p>Uh-oh! Looks like the page you requested couldn’t be found. This usually happens when there’s a spelling mistake in the address bar.</p>
+			<p>Try going <a href="/">back home</a>. If you keep having trouble, try contacting us at <a href="mailto:support@gateway-learning.com">support@gateway-learning.com</a>.
+		</div>
+		<div class="sidebar secondary-col island">
+			
+		</div>
+	</div>
+</div>
+<?php require_once('inc/footer.php'); ?>
