1b12e0ca0bef457f697433058916febd016d6941
Dan Eden <dan.eden@me.com>
2013-04-10 17:06:12 +0100

    Housekeeping

--- inc/profile.php
+++ inc/profile.php
@@ -1,9 +1,8 @@
 <h3>Profile Overview</h3>
-    <img class="alignleft avatar" src="<?php get_profile_pic() ?>" width="48" height="48">
-    <p class="standalone"><strong><?php get_user_fname(); ?> <?php get_user_sname(); ?></strong></p>
-    <p><a href="/edit-profile.php">Edit profile/settings</a></p>
-    <p class="standalone">Modules Completed: <?php get_modules_completed(); ?></p>
-    <p>Average Score: <?php get_average_score(); ?>%</p>
-    <p>Why not try...</p>
-    <?php get_recommended(); ?>
-    <hr>
+	<img class="alignleft avatar" src="<?php get_profile_pic() ?>" width="48" height="48">
+	<p class="standalone"><strong><?php get_user_fname(); ?> <?php get_user_sname(); ?></strong></p>
+	<p><a href="/edit-profile.php">Edit profile/settings</a></p>
+	<p class="standalone">Modules Completed: <?php get_modules_completed(); ?></p>
+	<p>Average Score: <?php get_average_score(); ?>%</p>
+	<hr>
+	<?php get_recommended(); ?>
