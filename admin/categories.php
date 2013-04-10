3da1c430b84393a47741fc7253c06229f345c9b2
Dan Eden <dan.eden@me.com>
2013-04-10 12:56:12 +0100

    Friendly fallback when there are no questions, categories, modules, or references

--- admin/categories.php
+++ admin/categories.php
@@ -14,24 +14,32 @@
 		<div class="main-col island">
 			<h1>Categories</h1>
 			<p>Welcome to the Categories Page. From here, you view the current categories and edit existing ones.</p>
-			<table class="table zebra admin-categories-list">
-				<thead>
-					<tr>
-						<th>Category Name</th>
-						<th>Actions</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-					$query=mysql_query("SELECT * FROM categorytable");
-					while ($result = mysql_fetch_assoc($query)) {
-					?>
-							<tr><td><a href="edit_category.php?cID=<?php echo $result['categoryID'] ?>"><?php echo $result['categoryTitle'] ?></a></td>
-							<td><a class="message-danger" href="inc/php/delete_category.php?Cid=<?php echo $result['categoryID']?>">Delete</a></td></tr>
-					<?php } ?>
-				</tbody>
-			</table>
-			<a class="alignright butt butt-primary" href="create_category.php">Create Category</a>
+			<?php
+				$query=mysql_query("SELECT * FROM categorytable");
+				if (mysql_fetch_assoc($query)) {
+			?>
+				<table class="table zebra admin-categories-list">
+					<thead>
+						<tr>
+							<th>Category Name</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+						$query=mysql_query("SELECT * FROM categorytable");
+						while ($result = mysql_fetch_assoc($query)) {
+						?>
+								<tr><td><a href="edit_category.php?cID=<?php echo $result['categoryID'] ?>"><?php echo $result['categoryTitle'] ?></a></td>
+								<td><a class="message-danger" href="inc/php/delete_category.php?Cid=<?php echo $result['categoryID']?>">Delete</a></td></tr>
+						<?php } ?>
+					</tbody>
+				</table>
+				<a class="alignright butt butt-primary" href="create_category.php">Create Category</a>
+			<?php } else { ?>
+				<hr>
+				<p class="intro promo">No categories yet. <a href="create_category.php">Add one now</a>.</p>
+			<?php } ?>
 		</div>
 	</div>
 </div>
