6d4bad06d038e23defc2d8efb86a27ae52b19fc4
Jonathan Walter <Jon.Walter.89@gmail.com>
2013-04-11 13:28:31 +0100

    Started profile page, added % to locked modules

--- inc/profile.php
+++ inc/profile.php
@@ -2,7 +2,8 @@
 	<img class="alignleft avatar" src="<?php get_profile_pic() ?>" width="48" height="48">
 	<p class="standalone"><strong><?php get_user_fname(); ?> <?php get_user_sname(); ?></strong></p>
 	<p><a href="/edit-profile.php">Edit profile/settings</a></p>
-	<p class="standalone">Modules Completed: <?php get_modules_completed(); ?></p>
+    <p class="standalone"><a href="/profile.php?ID=<?php echo get_user_id(); ?>">View full profile</a></p>
+	<p>Modules Completed: <?php get_modules_completed(); ?></p>
 	<p>Average Score: <?php get_average_score(); ?>%</p>
 	<hr>
 	<?php get_recommended(); ?>
